21f03a2c721b148ccb74da6263d62ea5f9d89aa9
Peter Zhang <30383377+greathope@users.noreply.github.com>
2019-08-19 11:15:39 +0800

    Update 200_number_of_islands.py

--- code/200_number_of_islands.py
+++ code/200_number_of_islands.py
@@ -21,3 +21,25 @@ class Solution:
         self.helper(visited, grid, i + 1, j)
         self.helper(visited, grid, i, j - 1)
         self.helper(visited, grid, i, j + 1)
+
+ # BFS
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]: return 0
+        res = 0
+        m, n = len(grid), len(grid[0])
+        visited = [[False for _ in range(n)] for _ in range(m)]
+        dirs = [(-1,0), (1, 0), (0, 1), (0, -1)]
+        for i in range(m):
+            for j in range(n):
+                if visited[i][j] or grid[i][j] == '0': continue
+                res += 1
+                q = [(i, j)]
+                while q:
+                    t = q.pop(0)
+                    for k in range(4):
+                        x, y = t[0] + dirs[k][0], t[1] + dirs[k][1]
+                        if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == '0' or visited[x][y]: continue
+                        visited[x][y] = True
+                        q.append((x,y))
+        return res
