20da9379ca6461b6b5c5d6ac46427a7321f57d6f
Peter Zhang <30383377+greathope@users.noreply.github.com>
2019-08-19 11:46:10 +0800

    Update 33_search_in_rotated_sorted_array.py

--- code/33_search_in_rotated_sorted_array.py
+++ code/33_search_in_rotated_sorted_array.py
@@ -1,7 +1,5 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if not nums:
-            return -1
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = left + (right - left) // 2
